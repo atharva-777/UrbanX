4bb3ea1a29332ea3fd49e5d940a6196bc0170db5
Atharva Jadhav <jadhavatharva499@gmail.com>
2024-02-12 23:47:30 +0530

    user services

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,33 @@
 import { Pool, Client } from "pg";
 const connectionString = process.env.DATABASE_URL;
+import { NextRequest, NextResponse } from "next/server";
+
+const pool = new Pool({
+  user: process.env.PGSQL_USER,
+  host: process.env.PGSQL_HOST,
+  password: process.env.PGSQL_PASSWORD,
+  database: process.env.PGSQL_DATABASE,
+  port: 5432,
+});
+
+export class UserService {
+  static async createUser(data: any, req: NextRequest, res: NextResponse) {
+    const { name, email } = data;
+
+    pool.query(
+      `INSERT INTO users (name, email) VALUES (${name},${email}) RETURNING *`,
+      [name, email],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        return NextResponse.json({
+          message: `User added with ID: ${results.rows[0].id}`,
+        });
+      }
+    );
+  }
+}
 
 const connect = async () => {
   const conn = new Pool({
