13335773f088e8c23bfa05231a3804cb1743a320
Atharva Jadhav <jadhavatharva499@gmail.com>
2024-03-10 23:33:46 +0530

    cart service

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.BACKEND_URL,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,15 +1,31 @@
 import { api } from "../config/api";
 
-class CartService{ 
-    static async addToCart(){
+class CartService {
+  static async addToCart(userId: number, productId: number, quantity: number) {
+    const res = await api.post("/cart/addToCart", {
+      userId,
+      productId,
+      quantity,
+    });
+    return res.data;
+  }
 
-    }
+  static async updateCart(userId: number, productId: number, quantity: number) {
+    const res = await api.post("/cart/updateCart", {
+      userId,
+      productId,
+      quantity,
+    });
+    return res.data;
+  }
 
-    static async removeFromCart(){
+  static async removeFromCart(userId: number, productId: number) {
+    const res = await api.post("/cart/removeFromCart", { userId, productId });
+    return res.data;
+  }
 
-    }
-
-    static async updateCart(){
-        
-    }
+  static async getCart(userId: number) {
+    const res = await api.post("/cart/getCart",{userId});
+    return res.data;
+  }
 }
